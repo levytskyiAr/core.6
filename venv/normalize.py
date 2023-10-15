baf244ce9d6bfbd54ffe4b8ab480eaed4b385e8b
Artur <arturlevuckuy@gmail.com>
2023-10-15 16:13:26 +0200

    second commit

--- venv/normalize.py
+++ venv/normalize.py
@@ -2,3 +2,5 @@ import re
 
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u","f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+
+
