f91b3b6b5a623a4258139c70c049a53a648fe2be
Artur <arturlevuckuy@gmail.com>
2023-10-15 16:07:11 +0200

    firs commit

---
+++ normalize.py
@@ -0,0 +1,4 @@
+import re
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u","f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
